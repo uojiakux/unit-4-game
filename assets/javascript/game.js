f353b0a3386b775c395da94f432427a1153c0981
USER <USER@BEP-LH9NND12.am.bracco.priv>
2018-07-28 23:52:08 -0500

    Saturday Night Push

---
+++ assets/javascript/game.js
@@ -0,0 +1,47 @@
+
+
+// Start the user's score at zero
+
+let results = 0;
+
+// Define the crystals and their values
+// You will need a Math.Random generator to change the values of each crystal image each time the window refreshes. 
+
+const bluecrystal = Math.floor(Math.random() * 12) + 1;
+const greencrystal = Math.floor(Math.random() * 12) + 1;
+const purplecrystal = Math.floor(Math.random() * 12) + 1;
+const redcrystal = Math.floor(Math.random() * 12) + 1;
+
+
+// Define the value of the target number
+
+    const targetNumber = Math.floor(Math.random() * 102) + 19;
+    $("#target-number").text(targetNumber)
+
+
+// You will need a on.("click", _____) event to define a function that will happen when the user clicks a certain image
+
+$("#blue-crystal").on("click", function () {
+
+    results = results + bluecrystal;
+    $("#results").text(results);
+})
+
+$("#green-crystal").on("click", function () {
+
+    results = results + greencrystal;
+    $("#results").text(results);
+})
+
+$("#purple-crystal").on("click", function () {
+
+    results = results + purplecrystal;
+    $("#results").text(results);
+})
+
+$("#red-crystal").on("click", function () {
+
+    results = results + redcrystal;
+    $("#results").text(results);
+})
+
